0244394e8e204d8e4a9cbfab4ceea01a8d9a2926
Botijas <dallazenanthony@gmail.com>
2026-03-25 14:14:12 -0400

    Finalização no Pause
    a. Menu de pause finalizado (em exceção  a opção de voltar ao menu, que será adicionado posteriormente junto a criação da cena de menu).
    b. Problema com o "grab_focus" nos botões corrigidos.
    c. Adição de atores temporários para teste do pause.

--- Assets/Scripts/game_manager.gd
+++ Assets/Scripts/game_manager.gd
@@ -3,34 +3,44 @@ extends Node
 enum GAME_STATES {
 	Playing, 
 	Losing,
-	Paused
-}
+	Paused}
 
-var actual_game_state : GAME_STATES = 0
+var actual_game_state : GAME_STATES = GAME_STATES.Playing
+
+@export var ui_animator : AnimationPlayer
 
 func _process(delta: float) -> void:
 	match actual_game_state:
 		#Playing
 		0:
-			pass
+			# O "await" é ultilizado aqui para garantir que o menu vai estar invisivel
+			await ui_animator.animation_finished
+			if get_tree().paused != false:
+				get_tree().paused = false
 		#Losing
 		1:
 			pass
 		#Paused
 		2:
-			pass
+			if get_tree().paused != true:
+				get_tree().paused = true
 		
 
 func _input(event: InputEvent) -> void:
+	# Este "if" verifica se o input pause foi precionado
 	if Input.is_action_just_pressed("Pause"):
-		pause_btn()
+		# Este "if" verifica se um animação não está sendo tocada 
+		if !ui_animator.is_playing():
+			# Esse "if" e "else" determinam para qual estado ele vai
+			if actual_game_state == GAME_STATES.Playing:
+				go_to_paused()
+			elif actual_game_state == GAME_STATES.Paused:
+				go_to_playing()
 
-func pause_btn() -> void:
-	get_tree().paused = !get_tree().paused
-	print(get_tree().paused)
-	if get_tree().paused:
-		actual_game_state = GAME_STATES.Paused
-	else:
-		actual_game_state = GAME_STATES.Playing
-	
-	
+func go_to_playing() -> void:
+	actual_game_state = GAME_STATES.Playing
+	ui_animator.play("Pause Down")
+
+func go_to_paused() -> void:
+	actual_game_state = GAME_STATES.Paused
+	ui_animator.play("Pause Up")
--- Assets/Scripts/ui_manager.gd
+++ Assets/Scripts/ui_manager.gd
@@ -1,6 +1,25 @@
 extends Control
 
+@export_category("Scenes Refs")
 @export var scene : Node
+@export_category("Buttons Refs")
+@export var pause_btn : TextureButton
+@export var resume_btn : Button
+@export var back_btn : Button
+@export var quit_btn : Button
 
 func _on_pause_btn_pressed() -> void:
-	scene.pause_btn()
+	scene.go_to_paused()
+	pause_btn.set_focus_mode(FOCUS_NONE)
+
+func _on_resume_btn_pressed() -> void:
+	scene.go_to_playing() 
+	resume_btn.set_focus_mode(FOCUS_NONE)
+
+func _on_back_btn_pressed() -> void:
+	
+	back_btn.set_focus_mode(FOCUS_NONE) 
+
+func _on_quit_btn_pressed() -> void:
+	get_tree().quit() 
+	quit_btn.set_focus_mode(FOCUS_NONE)
