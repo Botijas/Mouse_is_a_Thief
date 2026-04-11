869ecae580bb490dfcac1a1e0b5a7ecd606a3cad
Botijas <dallazenanthony@gmail.com>
2026-04-10 20:18:21 -0400

    Implementação do sistema de salvamento de dado
    a. High score sendo salvo num arquivo JSON.

--- Assets/Scripts/game_manager.gd
+++ Assets/Scripts/game_manager.gd
@@ -5,28 +5,33 @@ enum GAME_STATES {
 	Losing,
 	Paused}
 
-var actual_game_state : GAME_STATES = GAME_STATES.Playing
+var actual_game_state : GAME_STATES
 
 var points : int = 0
+var high_score : int = 0
+
+var data_path = "user://data.json"
 
 @export var ui_animator : AnimationPlayer
+@export var ui_manager : Control
 
 signal lose(to: String)
 
 func _ready() -> void:
+	read_data()
+	actual_game_state = GAME_STATES.Playing
 	lose.connect(_on_lose)
 
 func _process(delta: float) -> void:
 	match actual_game_state:
 		#Playing
 		0:
-			# O "await" é ultilizado aqui para garantir que o menu vai estar invisivel
-			await ui_animator.animation_finished
 			if get_tree().paused != false:
 				get_tree().paused = false
 		#Losing
 		1:
-			pass
+			if get_tree().paused != true:
+				get_tree().paused = true
 		#Paused
 		2:
 			if get_tree().paused != true:
@@ -46,17 +51,55 @@ func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		points += 1
 
+func save_data() -> void:
+	var data := {
+		"high_score" : max(points, high_score) }
+	
+	var archive = FileAccess.open(data_path, FileAccess.WRITE)
+	if archive == null:
+		printerr("Archive not found")
+		return
+	
+	var json = JSON.stringify(data)
+	archive.store_string(json)
+	archive.close()
+
+func read_data() -> void:
+	if not FileAccess.file_exists(data_path):
+		printerr("Não Existe Dado aqui")
+		return
+	
+	var archive = FileAccess.open(data_path, FileAccess.READ)
+	if archive == null:
+		printerr("Archive not found")
+		return
+	
+	var content = archive.get_as_text()
+	archive.close()
+	
+	var json = JSON.parse_string(content)
+	if not json:
+		printerr("Não foi possível abrir o JSON")
+		return
+	
+	high_score = json["high_score"]
+
 func go_to_playing() -> void:
-	actual_game_state = GAME_STATES.Playing
 	ui_animator.play("Pause Down")
+	# O "await" é ultilizado aqui para garantir que o menu vai estar invisivel
+	await ui_animator.animation_finished
+	actual_game_state = GAME_STATES.Playing
 
 func go_to_paused() -> void:
-	actual_game_state = GAME_STATES.Paused
 	ui_animator.play("Pause Up")
+	actual_game_state = GAME_STATES.Paused
+
+func go_to_losing(to: String) -> void:
+	ui_animator.play("Lose Down")
+	read_data()
+	ui_manager.set_scores_on_labels()
+	actual_game_state = GAME_STATES.Losing
 
 func _on_lose(to: String):
-	match to:
-		"wait_time":
-			print("Você ficou parado")
-		"civil":
-			print("Você esbarrou no Civil")
+	save_data()
+	go_to_losing(to)
--- Assets/Scripts/ui_manager.gd
+++ Assets/Scripts/ui_manager.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @export_category("Scenes Refs")
-@export var scene : Node
+@export var game_scene : Node
 @export_category("Buttons Refs")
 @export var pause_btn : BaseButton
 @export var resume_btn : BaseButton
@@ -10,16 +10,22 @@ extends Control
 @export var restart_btn : BaseButton
 @export_category("Score Refs")
 @export var score : Label
+@export var last_score : Label
+@export var high_score : Label
 
 func _physics_process(delta: float) -> void:
-	score.text = str(scene.points)
+	score.text = str(game_scene.points)
+
+func set_scores_on_labels() -> void:
+	last_score.text = str(game_scene.points)
+	high_score.text = str(game_scene.high_score)
 
 func _on_pause_btn_pressed() -> void:
-	scene.go_to_paused()
+	game_scene.go_to_paused()
 	pause_btn.set_focus_mode(FOCUS_NONE)
 
 func _on_resume_btn_pressed() -> void:
-	scene.go_to_playing() 
+	game_scene.go_to_playing() 
 	resume_btn.set_focus_mode(FOCUS_NONE)
 
 func _on_back_btn_pressed() -> void:
@@ -31,4 +37,5 @@ func _on_quit_btn_pressed() -> void:
 
 func _on_restart_btn_pressed() -> void:
 	get_tree().reload_current_scene()
+	game_scene.read_data()
 	restart_btn.set_focus_mode(FOCUS_NONE)
